a1542ae05b658090e91d4c865840954372d18552
austinchitmon <austinchitmon@gmail.com>
2020-01-17 14:55:12 -0600

    begin opening csv file

--- Project1/src/FileManager.java
+++ Project1/src/FileManager.java
@@ -1,9 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
 public class FileManager {
     String[] fields = {"RANK", "NAME", "CITY", "STATE", "ZIP", "EMPLOYEES"};
     Record currentRecord = new Record();
+    Scanner input = new Scanner(System.in);
 
     public void createNewDatabase() {
-        System.out.print("Creating new db... \n");
+        boolean hasError = false;
+        String fileName = null;
+
+
+        do {
+            try {
+                System.out.print("Enter name of .csv file to create DB from (omit extension): ");
+                fileName = input.nextLine();
+                RandomAccessFile csvFile = new RandomAccessFile(fileName + ".csv", "r");
+                hasError = false;
+            } catch(FileNotFoundException e) {
+                System.out.printf("Error, file not found: %s \n", e.toString());
+                hasError = true;
+            }
+
+        } while(hasError);
     }
 
     public void openDatabase() {
